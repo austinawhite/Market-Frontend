c50b51736a820802f2b1d523a41eb75c4a2b4b7f
ILIAN ILIEV <iiliev84@gmail.com>
2025-06-11 21:25:27 -0500

    added functions names in App,Login and Register and updated main.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,2 +1,11 @@
 import {useState} from 'react'
 import { useNavigate, Link } from 'react-router-dom';
+
+function LogIn({setToken}){
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState('');
+    
+}
+export default LogIn
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,2 +1,11 @@
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+
+function Register() {
+    const navigate = useNavigate();
+    const [username, setUsername] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState('');
+
+}
+export default Register
