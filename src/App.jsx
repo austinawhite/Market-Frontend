1254f01bcf9092689823c60d3ef3170d96677e5d
ILIAN ILIEV <iiliev84@gmail.com>
2025-06-14 12:14:54 -0500

    added Register functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import {Route, Routes} from 'react-router-dom'
 import './App.css'
+import Register from './components/Register'
+import LogIn from './components/Login'
 
 function App() {
   const [products, setProducts] = useState(0)
+  const [token, setToken] = useState(() => localStorage.getItem("token"));
+
+  useEffect(() => {
+    if (token) {
+      localStorage.setItem("token", token);
+    } else {
+      localStorage.removeItem("token");
+    }
+  }, [token]);
 
   return (
     <>
     <div>
       <Routes>
-      <Route path="/" element={<h1>Welome</h1>}/>
+        <Route path="/" element={<h1>Welome</h1>}/>
+        <Route path='/login' element={<LogIn setToken={setToken} />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
     <div>
-      <h1>This will be our homepage where our products will be displayed to the customers.</h1>
+
     </div>
     </>
   )
--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -7,5 +7,61 @@ function Register() {
     const [password, setPassword] = useState("")
     const [error, setError] = useState('');
 
+    async function handleSubmit(e){
+        e.preventDefault();
+        setError("");        
+
+        try {
+            const response = await fetch('http://localhost:3000/users/', {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                username: username,
+                password: password,
+                }),
+              });
+              
+            const result = await response.json();
+            if (response.ok) {
+              navigate('/login');
+            } else {
+              setError("Failed to register" || result.error);
+            }
+          } catch (error) {
+            console.error(error);
+            setError(error.message);
+          }
+        }     
+        
+        return(
+            <>
+            <div className="register-container">
+            <h2>Register</h2>
+            {error && <p>{error}</p>}
+            <form onSubmit={handleSubmit}>
+                <label>
+                    Username: <input 
+                    value = {username}
+                    required
+                    onChange={(e) => setUsername(e.target.value)} />
+                </label>
+                <label>
+                    Password: <input 
+                    type="password" 
+                    value={password} 
+                    minLength={5}
+                    title="Your password must be at least 5 characters long."
+                    required 
+                    onChange={(e) => setPassword(e.target.value)} />
+                </label>
+                <button className="register-button">Submit</button>
+                <p><button><Link className="nav-link"to="/login">Login</Link> </button> if you have an account.</p>
+            </form>
+            </div>
+            </>
+          );
 }
+
 export default Register
