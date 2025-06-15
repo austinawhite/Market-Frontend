48d62f084a88183abf41241288e13188f7b3611e
Austina White <austinawhite@gmail.com>
2025-06-15 11:29:36 -0400

    starting form and list for reviews

---
+++ src/components/ReviewForm.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+//This component is shown to users who have previously purchased the product, giving them the ability to leave a review. 
+
+
+export default function ReviewForm() {
+
+const [rating, setRating] = useState(0);
+const [hoverRating, setHoverRating] = useState(0);
+const [comment, setComment] = useState ('');
+
+const handleSubmit = () => 
+
+
+return (
+<form submit="On Click"> 
+</form> 
+
+);
