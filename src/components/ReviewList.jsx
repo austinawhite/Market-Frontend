6e3aabb8387a7ce79b123dc71b4ce2dd45a98854
Austina White <austinawhite@gmail.com>
2025-06-17 21:44:43 -0400

    Review list updates

--- src/components/ReviewList.jsx
+++ src/components/ReviewList.jsx
@@ -1,5 +1,3 @@
-import { useState } from "react";
-
 // This component will be used to show the user the product reviews that they previously submitted. It also gives them the ability to edit and delete the product review. 
 
 //! Show the product reviews in order by date submitted. 
