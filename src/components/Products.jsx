f280e375a4c4ea32c205c7ae5eea5c23eb552213
Trisha Goodwin <trishabromero@gmail.com>
2025-06-17 19:22:52 -0500

    Took details out of whole book list so it would have somehting to add when they clicked the details button.

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -30,7 +30,6 @@ return(
             products.map((product)=>
                 <div key={product.id}>
                     <h2>{product.title}</h2>
-                    <h3>{product.description}</h3>
                     <h3>{product.price}</h3>
                     <div>
                         <button onClick={()=> handleClick(product)}>Details</button>
